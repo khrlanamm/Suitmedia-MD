668a953f38d3c9fda8e6dc3f7e51ff4e47bdb552
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2025-02-13 10:15:30 +0700

    UI Basic Build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.khrlanamm.suitmediamd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.khrlanamm.suitmediamd"
--- app/src/main/java/com/khrlanamm/suitmediamd/ui/second/SecondScreenActivity.kt
+++ app/src/main/java/com/khrlanamm/suitmediamd/ui/second/SecondScreenActivity.kt
@@ -1,16 +1,17 @@
-package com.khrlanamm.suitmediamd
+package com.khrlanamm.suitmediamd.ui.second
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.khrlanamm.suitmediamd.R
 
-class MainActivity : AppCompatActivity() {
+class SecondScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
